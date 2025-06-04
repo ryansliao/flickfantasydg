5824b92923140ef946b3e30654584023f6c5b98f
ryansliao <ryansichenliao@gmail.com>
2025-06-03 17:45:11 -0700

    Player Add

--- fantasydg/obj/Debug/net8.0/fantasydg.AssemblyInfo.cs
+++ fantasydg/obj/Debug/net8.0/fantasydg.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fantasydg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c35164fd3dddf886b12062af4ba0272308c8aa5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38990e0d4af3effe991c977a01af15c284714f2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("fantasydg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fantasydg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
