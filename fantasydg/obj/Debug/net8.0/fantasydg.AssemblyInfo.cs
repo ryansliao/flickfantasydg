a8f941f762bb4593f590392718f89a82b8cd32ec
ryansliao <ryansichenliao@gmail.com>
2025-06-25 08:38:05 -0700

    empty data view bug fix

--- fantasydg/obj/Debug/net8.0/fantasydg.AssemblyInfo.cs
+++ fantasydg/obj/Debug/net8.0/fantasydg.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fantasydg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c55c408c3dfe187530ce9a540a5e6ed674d92f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f433831138b1f11e3eeae22c313a1c4fef731bd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("fantasydg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fantasydg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
